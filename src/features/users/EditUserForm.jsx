72c9e62639ca5657b159a9043055edc0c4339cf7
myladd <mylad.zandkarimi@gmail.com>
2023-07-20 16:22:14 +0330

    styled users page

--- src/features/users/EditUserForm.jsx
+++ src/features/users/EditUserForm.jsx
@@ -1,9 +1,8 @@
 import { useState, useEffect } from "react";
 import { useUpdateUserMutation, useDeleteUserMutation } from "./usersApiSlice";
 import { useNavigate } from "react-router-dom";
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { faSave, faTrashCan } from "@fortawesome/free-solid-svg-icons";
 import { ROLES } from "../../config/roles";
+import { Button, Checkbox, InputLabel, MenuItem, OutlinedInput, Select, TextField } from "@mui/material";
 
 const USER_REGEX = /^[A-z]{3,20}$/;
 const PWD_REGEX = /^[A-z0-9!@#$%]{4,12}$/;
@@ -88,41 +87,50 @@ const EditUserForm = ({ user }) => {
   const content = (
     <>
       <p className={errClass}>{errContent}</p>
-
-      <form className="form" onSubmit={(e) => e.preventDefault()}>
-        <div className="form__title-row">
-          <h2>Edit User</h2>
-          <div className="form__action-buttons">
-            <button className="icon-button" title="Save" onClick={onSaveUserClicked} disabled={!canSave}>
-              save
-            </button>
-            <button className="icon-button" title="Delete" onClick={onDeleteUserClicked}>
-              trash
-            </button>
+      <div className="form-container w-full mt-12 flex flex-col items-center justify-center">
+        <form className="form mt-8 w-96 flex flex-col items-center gap-4" onSubmit={(e) => e.preventDefault()}>
+          <div className="form__title-row w-96 p-4 rounded-md bg-emerald-400 flex justify-center text-cyan-800">
+            <h2 className="text-xl font-bold">Edit User</h2>
           </div>
-        </div>
-        <label className="form__label" htmlFor="username">
-          Username: <span className="nowrap">[3-20 letters]</span>
-        </label>
-        <input className={`form__input ${validUserClass}`} id="username" name="username" type="text" autoComplete="off" value={username} onChange={onUsernameChanged} />
+          <div className="username-field w-full">
+            <label className="form__label" htmlFor="username">
+              Username: <span className="nowrap">[3-20 letters]</span>
+            </label>
+            <TextField className={`form__input w-full ${validUserClass}`} id="username" name="username" type="text" autoComplete="off" value={username} onChange={onUsernameChanged} />
+          </div>
 
-        <label className="form__label" htmlFor="password">
-          Password: <span className="nowrap">[empty = no change]</span> <span className="nowrap">[4-12 chars incl. !@#$%]</span>
-        </label>
-        <input className={`form__input ${validPwdClass}`} id="password" name="password" type="password" value={password} onChange={onPasswordChanged} />
+          <div className="password-field">
+            <label className="form__label" htmlFor="password">
+              Password: <span className="nowrap">[empty = no change]</span>
+              <br />
+              <span className="nowrap">[4-12 chars incl. !@#$%]</span>
+            </label>
+            <TextField className={`form__input w-full ${validPwdClass}`} id="password" name="password" type="password" value={password} onChange={onPasswordChanged} />
+          </div>
 
-        <label className="form__label form__checkbox-container" htmlFor="user-active">
-          ACTIVE:
-          <input className="form__checkbox" id="user-active" name="user-active" type="checkbox" checked={active} onChange={onActiveChanged} />
-        </label>
+          <label className="form__label form__checkbox-container" htmlFor="user-active">
+            ACTIVE:
+            <Checkbox className="form__checkbox" id="user-active" name="user-active" type="checkbox" checked={active} onChange={onActiveChanged} defaultChecked />
+          </label>
 
-        <label className="form__label" htmlFor="roles">
-          ASSIGNED ROLES:
-        </label>
-        <select id="roles" name="roles" className={`form__select ${validRolesClass}`} multiple={true} size="3" value={roles} onChange={onRolesChanged}>
-          {options}
-        </select>
-      </form>
+          <div className="select-container flex items-center gap-2">
+            <label className="form__label" htmlFor="roles">
+              ASSIGNED ROLES:
+            </label>
+            <select id="roles" name="roles" className={`form__select ${validRolesClass}`} multiple={true} size="3" value={roles} onChange={onRolesChanged}>
+              {options}
+            </select>
+          </div>
+          <div className="form__action-buttons w-full flex gap-2">
+            <Button className="icon-button w-full" variant="contained" color="success" title="Save" disabled={!canSave}>
+              save
+            </Button>
+            <Button className="icon-button w-full" variant="contained" color="error" title="Delete" onClick={onDeleteUserClicked}>
+              trash
+            </Button>
+          </div>
+        </form>
+      </div>
     </>
   );
 
--- src/features/users/NewUserForm.jsx
+++ src/features/users/NewUserForm.jsx
@@ -1,12 +1,23 @@
 import { useState, useEffect } from "react";
 import { useAddNewUserMutation } from "./usersApiSlice";
 import { useNavigate } from "react-router-dom";
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { faSave } from "@fortawesome/free-solid-svg-icons";
 import { ROLES } from "../../config/roles";
+import { Button, InputLabel, MenuItem, OutlinedInput, Select, TextField } from "@mui/material";
 
 const USER_REGEX = /^[A-z]{3,20}$/;
 const PWD_REGEX = /^[A-z0-9!@#$%]{4,12}$/;
+
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+};
+
 
 const NewUserForm = () => {
   const [addNewUser, { isLoading, isSuccess, isError, error }] = useAddNewUserMutation();
@@ -58,10 +69,10 @@ const NewUserForm = () => {
 
   const options = Object.values(ROLES).map((role) => {
     return (
-      <option key={role} value={role}>
+      <MenuItem key={role} value={role}>
         {" "}
         {role}
-      </option>
+      </MenuItem>
     );
   });
 
@@ -73,33 +84,43 @@ const NewUserForm = () => {
   const content = (
     <>
       <p className={errClass}>{error?.data?.message}</p>
-
-      <form className="form" onSubmit={onSaveUserClicked}>
-        <div className="form__title-row">
-          <h2>New User</h2>
-          <div className="form__action-buttons">
-            <button className="icon-button" title="Save" disabled={!canSave}>
-              save
-            </button>
-          </div>
+      <div className="form-container w-full mt-12 flex flex-col items-center justify-center">
+        <div className="form__title-row w-80 p-4 rounded-md bg-emerald-400 flex justify-center text-cyan-800">
+          <h2 className="text-xl font-bold">New User</h2>
         </div>
-        <label className="form__label" htmlFor="username">
-          Username: <span className="nowrap">[3-20 letters]</span>
-        </label>
-        <input className={`form__input ${validUserClass}`} id="username" name="username" type="text" autoComplete="off" value={username} onChange={onUsernameChanged} />
+        <form className="form mt-8 w-80 flex flex-col items-center gap-4" onSubmit={onSaveUserClicked}>
+          <div className="username-field">
+            <label className="form__label" htmlFor="username">
+              Username: <span className="nowrap">[3-20 letters]</span>
+            </label>
+            <TextField label="Username" variant="filled" className={`form__input w-full ${validUserClass}`} id="username" name="username" type="text" autoComplete="off" value={username} onChange={onUsernameChanged} />
+          </div>
 
-        <label className="form__label" htmlFor="password">
-          Password: <span className="nowrap">[4-12 chars incl. !@#$%]</span>
-        </label>
-        <input className={`form__input ${validPwdClass}`} id="password" name="password" type="password" value={password} onChange={onPasswordChanged} />
-
-        <label className="form__label" htmlFor="roles">
-          ASSIGNED ROLES:
-        </label>
-        <select id="roles" name="roles" className={`form__select ${validRolesClass}`} multiple={true} size="3" value={roles} onChange={onRolesChanged}>
-          {options}
-        </select>
-      </form>
+          <div className="password-field">
+            <label className="form__label" htmlFor="password">
+              Password: <span className="nowrap">[4-12 chars incl. !@#$%]</span>
+            </label>
+            <TextField label="Password" variant="filled" className={`form__input w-full ${validPwdClass}`} id="password" name="password" type="password" autoComplete="off" value={password} onChange={onPasswordChanged} />
+          </div>
+          <div className="select-container flex items-center gap-2">
+            {/* <label className="form__label" htmlFor="roles">
+              ASSIGNED ROLES:
+            </label> */}
+            <InputLabel id="roles">Role: </InputLabel>
+            <Select className={`form__select ${validRolesClass}`} MenuProps={MenuProps} input={<OutlinedInput label="role" />} labelId="roles" id="roles" name="roles" value={roles} label="Role" multiple onChange={onRolesChanged}>
+              {options}
+            </Select>
+            {/* <select id="roles" name="roles" className={`form__select ${validRolesClass}`} multiple={true} size="3" value={roles} onChange={onRolesChanged}>
+              {options}
+            </select> */}
+          </div>
+          <div className="form__action-buttons w-full">
+            <Button className="icon-button w-full" variant="contained" color="success" title="Save" disabled={!canSave}>
+              save
+            </Button>
+          </div>
+        </form>
+      </div>
     </>
   );
 
